8e7d7f07ee522c7ed1adda9ca5996d7360bac5d6
kiboma256 <faithcecilia6@gmail.com>
2026-05-13 03:07:58 -0300

    Add working password reset with Firebase

--- attendance/JS/config.js
+++ attendance/JS/config.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   appId: "1:1039686457340:web:a0a5ff26c199f2596e0479",
   measurementId: "G-TC9RJST6F2"
 };
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const auth = firebase.auth();
-console.log("");
